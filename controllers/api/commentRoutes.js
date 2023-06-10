d960482aeaa0f2a4cccb9da2f36c7c05f0964929
Sean Zweifel <spzweifel@gmail.com>
2023-06-10 16:02:20 -0400

    handlebars and readme left

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,43 @@
+const router = require("express").Router();
+const { Comment } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const { text, postId } = req.body;
+    const userId = req.session.user_id;
+
+    const commentData = await Comment.create({
+      comments: text,
+      user_id: userId,
+      post_id: postId,
+    });
+
+    res.status(200).json({ message: "Comment created successfully" });
+    return;
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete("/:id", withAuth, async (req, res) => {
+  try {
+    const commentDelete = await Comment.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!commentDelete) {
+      res.status(404).json({ message: "No comment found with this id!" });
+      return;
+    }
+
+    res.status(200).json({ message: "Comment deleted successfully!" });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
---
+++ public/js/comment.js
@@ -0,0 +1,55 @@
+const newCommentHandler = async (event) => {
+    event.preventDefault();
+    
+    const text = document.querySelector('#comment-textarea').value.trim();
+    const postId = document.location.pathname.split('/').pop();
+    
+    if (text && postId) {
+      try {
+        const response = await fetch('/api/comments', {
+          method: 'POST',
+          body: JSON.stringify({ text, postId }),
+          headers: {
+            'Content-Type': 'application/json',
+            },
+         });
+    
+        if (response.ok) {
+          const commentData = await response.json();
+          console.log('Comment created:', commentData);
+          document.location.reload();
+  
+        } else {
+          console.error('Failed to create comment:', response.status);
+          document.location.href = document.location.origin + '/login';
+        }
+      } catch (error) {
+        console.error('Error creating comment:', error);
+        document.location.href = document.location.origin + '/login';
+        }
+    }
+  };
+  
+  const delCommentsHandler = async (event) => {
+    if (event.target.hasAttribute("data-id")) {
+      const id = event.target.getAttribute("data-id");
+  
+      const response = await fetch(`/api/comments/${id}`, {
+        method: "DELETE",
+      });
+  
+      if (response.ok) {
+        document.location.reload()
+      } else {
+        alert("Failed to delete comment");
+      }
+    }
+  };
+  
+  document
+    .querySelector('#comment-submit')
+    .addEventListener('click', newCommentHandler);
+  
+  document
+    .querySelector('#delcomment')
+    .addEventListener('click', delCommentsHandler);
